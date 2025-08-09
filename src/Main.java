b3063edf24552ec4e0d093b527eff62eb39a3b12
anndrem <dealmeida.andre08@gmail.com>
2025-08-09 19:51:31 -0300

    refractor: otimizando o código

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,24 @@
 
 public class Main {
     static void verifyNumbers(int[] nums) {
-        int k = 0;
         int j = 1;
         int won = 0;
-        while (k < nums.length - 1) {
-            for (int i = k + 1; i < nums.length; i++) {
-                if (nums[k] == nums[i]) {
+        for (int k : nums) {
+            for (int num : nums) {
+                if (k == num) {
                     j++;
                     if (j > nums.length / 2) {
-                        won = nums[k];
+                        won = k;
                     }
                 }
             }
-            j = 0;
-            k++;
+            j = 1;
         }
         System.out.print(won);
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,1,2,1};
+        int[] nums = {2,2,2,1};
         verifyNumbers(nums);
     }
 }
